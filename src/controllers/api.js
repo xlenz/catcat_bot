68b916f7c5f4c53884dddcf89c2fbd7f4343567d
MG <xlenz7@gmail.com>
2015-08-02 01:02:46 +0300

    send pic
    002

--- src/controllers/api.js
+++ src/controllers/api.js
@@ -2,18 +2,31 @@
 
 var self = this;
 var cfg = null;
+var token = null;
+var TelegramBot = require('node-telegram-bot-api');
+var catcatBot = new TelegramBot(token, {polling: false});
 
-module.exports = function (_cfg) {
+module.exports = function (_cfg, _token) {
   cfg = _cfg;
+  token = _token;
   return self;
 };
 
 exports.hello = function (req, res, next) {
-  return res.send('This is @catcat_bot api server. Please find us in telegram.');
+  return res.send('This is @catcat_bot api server. Please find us in telegram. v002');
 };
 
 exports.catcat_bot = function (req, res, next) {
-    log.info(req.body);
-    log.trace('trace');
-    return res.send();
-}
+  try {
+    var message = req.body.message;
+    var updateId = req.body.update_id;
+    console.log(message);
+    log.verbose(updateId);
+    var chatId = message.chat.id;
+    var photo = cfg.rootDir + 'cats-87a.jpg';
+    catcatBot.sendPhoto(chatId, photo, {caption: "I'm a bot!"});
+  } catch (exception) {
+    log.error(exception);
+  }
+  return res.send();
+};
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,15 @@
 'use strict';
 
 var self = this;
+var token = '91219955:AAEIbiz947It9VaFEyvjelh0ni7lELYZqLc';
 var api;
 
 module.exports = function (cfg) {
-  api = require('./controllers/api')(cfg);
+  api = require('./controllers/api')(cfg, token);
   return self;
 };
 
 exports.routes = function (app) {
   app.get('/', api.hello);
-  app.post('/bot/91219955:AAEIbiz947It9VaFEyvjelh0ni7lELYZqLc', api.catcat_bot);
+  app.post('/bot/' + token, api.catcat_bot);
 };
