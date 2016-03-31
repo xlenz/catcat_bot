1cad8c51b71f0f2d16605ee026deafde9e14fb6e
MG <xlenz7@gmail.com>
2016-04-01 02:19:56 +0300

    try increase stability

--- src/controllers/api.js
+++ src/controllers/api.js
@@ -2,6 +2,11 @@
 var request = require('request');
 var TelegramBot = require('node-telegram-bot-api');
 var fs = require('fs');
+var imagesFolder = 'images';
+
+fs.exists(imagesFolder, (exists) => {
+  if (!exists) fs.mkdir(imagesFolder);
+});
 
 var self = this;
 var cfg = null;
@@ -23,7 +28,7 @@ exports.catcat_bot = function (req, res, next) {
   try {
     var message = req.body.message;
     if (!message || !message.text) {
-      console.log(req.body);
+      //console.log(req.body);
       return res.send();
     }
     //var updateId = req.body.update_id;
@@ -39,25 +44,28 @@ exports.catcat_bot = function (req, res, next) {
     if (message.text.toLowerCase().indexOf('/cat') !== 0) {
       return res.send();
     }
-    log.info(chatId);
-    var page = Math.floor(Math.random() * 999);
+    //log.info(chatId);
+    var page = Math.floor(Math.random() * 99);
     var sorts = ["votes_count", "rating", "times_viewed", "favorites_count", "comments_count"];
     var sort = sorts[Math.floor(Math.random() * sorts.length)];
     var catUrl = "https://api.500px.com/v1/photos/search?consumer_key=VbiGx68xIs98oeeSCfWMVqOHmC4K45OBxwgaakMn&tag=cat&rpp=1&sort=" + sort + "&image_size=4&page=" + page;
-    request.get(catUrl, function (err, response) {
-      var photoObj = JSON.parse(response.body).photos[0];
-      var imgUrl = photoObj.image_url;
-      var imgId = photoObj.id;
-      var imgName = 'img-' + imgId + '-' + chatId + '.jpg';
-      var fsStream = fs.createWriteStream(imgName);
-      fsStream.on('close', function() {
-        catcatBot.sendPhoto(chatId, imgName).then(function() {
-          fs.unlink(imgName);
+    (function (_chatId, _catUrl) {
+      request.get(_catUrl, function (err, response) {
+        var photoObj = JSON.parse(response.body).photos[0];
+        var imgName = imagesFolder + '/img-' + photoObj.id + '-' + _chatId + '.jpg';
+        fs.exists(imgName, (exists) => {
+          if (exists) imgName += Math.floor(Math.random() * 9999) + '.jpg';
+          var fsStream = fs.createWriteStream(imgName);
+          fsStream.on('close', function() {
+            catcatBot.sendPhoto(_chatId, imgName).then(function() {
+              fs.unlink(imgName);
+            });
+          });
+
+          request.get(photoObj.image_url).pipe(fsStream);
         });
       });
-
-      request.get(imgUrl).pipe(fsStream);
-    });
+    })(chatId, catUrl);
 
   } catch (exception) {
     log.error(exception);
